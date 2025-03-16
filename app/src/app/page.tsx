18b0e96c0f94058c07212582108d99dcc83fe006
SevvyP <longisaac06@gmail.com>
2025-03-15 23:20:01 -0500

    responsive to fit all screen sizes

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Debate() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 p-4">
       <div className="flex flex-col w-full bg-white shadow-md rounded-lg p-4">
-        <div className="flex flex-col overflow-y-auto h-96">
+        <div className="flex flex-col overflow-y-auto max-h-[50vh]">
           {messages.map((message, index) => (
             <div key={index} className="p-2 bg-gray-100 rounded-lg mb-2">
               <p className="text-black">
